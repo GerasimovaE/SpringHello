9699de75ed9fe96530d76cd66b20f88f5cece06a
Gerasimova Ekaterina <4217582@gmail.com>
2021-08-27 12:06:31 +0300

    2.2.1V2

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -12,44 +12,44 @@ import java.util.List;
 
 public class MainApp {
    public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+      AnnotationConfigApplicationContext context =
+              new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
 
-       List<Car> cars = new ArrayList<>();
-       User user1 = new User("User1", "Lastname1", "user1@mail.ru");
-       Car car1 = new Car("Ford", 1);
-       user1.setCar(car1);
-       car1.setUser(user1);
-       cars.add(car1);
-       userService.add(user1);
+      List<Car> cars = new ArrayList<>();
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      Car car1 = new Car("Ford", 1);
+      user1.setCar(car1);
+      car1.setUser(user1);
+      cars.add(car1);
+      userService.add(user1);
 
-       User user2 = new User("User2", "Lastname2", "user2@mail.ru");
-       Car car2 = new Car("Ford2", 2);
-       user2.setCar(car2);
-       car2.setUser(user2);
-       cars.add(car2);
-       userService.add(user2);
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      Car car2 = new Car("Ford2", 2);
+      user2.setCar(car2);
+      car2.setUser(user2);
+      cars.add(car2);
+      userService.add(user2);
 
-       User user3 = new User("User3", "Lastname3", "user3@mail.ru");
-       Car car3 = new Car("Ford3", 3);
-       user3.setCar(car3);
-       car3.setUser(user3);
-       cars.add(car3);
-       userService.add(user3);
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+      Car car3 = new Car("Ford3", 3);
+      user3.setCar(car3);
+      car3.setUser(user3);
+      cars.add(car3);
+      userService.add(user3);
 
-       for (Car car : cars) {
-           User userGet = userService.get(car.getModel(), car.getSeries());
+      for (Car car : cars) {
+         User userGet = userService.get(car.getModel(), car.getSeries());
 
-           System.out.println("Id = "+userGet.getId());
-           System.out.println("First Name = " + userGet.getFirstName());
-           System.out.println("Last Name = " + userGet.getLastName());
-           System.out.println("Email = " + userGet.getEmail());
-           System.out.println("Car model = " + car.getModel());
-           System.out.println("Car series = " + car.getSeries());
-           System.out.println();
-       }
+         System.out.println("Id = " + userGet.getId());
+         System.out.println("First Name = " + userGet.getFirstName());
+         System.out.println("Last Name = " + userGet.getLastName());
+         System.out.println("Email = " + userGet.getEmail());
+         System.out.println("Car model = " + car.getModel());
+         System.out.println("Car series = " + car.getSeries());
+         System.out.println();
+      }
 
       context.close();
    }
--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -3,30 +3,39 @@ package hiber.dao;
 import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.TypedQuery;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
+//   @Autowired
+//   private SessionFactory sessionFactory;
+
    @Autowired
-   private SessionFactory sessionFactory;
+   private EntityManagerFactory entityManagerFactory;
 
    @Override
    public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
+      EntityManager entityManager = entityManagerFactory.createEntityManager();
+      entityManager.getTransaction().begin();
+      entityManager.persist(user);
+      entityManager.getTransaction().commit();
+//      sessionFactory.getCurrentSession().save(user);
    }
 
    @Override
    public User get(String model, int series) {
-      Query query = sessionFactory.getCurrentSession().createQuery("from Car where model = :model and series = :series");
+      EntityManager entityManager = entityManagerFactory.createEntityManager();
+      entityManager.getTransaction().begin();
+      Query query = entityManager.createQuery("select m from Car m where m.model = :model and m.series = :series");
       query.setParameter("model", model);
       query.setParameter("series", series);
-
       List<Car> list = query.getResultList();
 
       return list.get(0).getUser();
@@ -35,8 +44,13 @@ public class UserDaoImp implements UserDao {
    @Override
    @SuppressWarnings("unchecked")
    public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
+      EntityManager entityManager = entityManagerFactory.createEntityManager();
+      entityManager.getTransaction().begin();
+      List list = entityManager.createQuery("select m from User m", User.class).getResultList();
+
+      return (List<User>) list;
+//      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+//      return query.getResultList();
    }
 
 }
